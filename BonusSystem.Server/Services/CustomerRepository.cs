466dea929882cb1e6a01fac3ac9bf30385704f99
Maxim <maxim.khachatryan.2013@gmail.com>
2020-03-04 23:14:44 +0400

    Add project files.

---
+++ BonusSystem.Server/Services/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using BonusSystem.Server.Models;
+
+namespace BonusSystem.Server.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void Create(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Get(Func<Customer, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetWithInclude(params Expression<Func<Customer, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetWithInclude(Func<Customer, bool> predicate, params Expression<Func<Customer, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BonusSystem.Server/Services/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using BonusSystem.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BonusSystem.Server.Services
+{
+    public interface ICustomerRepository
+    {
+        void Create(Customer item);
+        Customer FindById(int id);
+        IEnumerable<Customer> Get();
+        IEnumerable<Customer> Get(Func<Customer, bool> predicate);
+        bool Remove(Customer item);
+        IEnumerable<Customer> GetWithInclude(params Expression<Func<Customer, object>>[] includeProperties);
+        IEnumerable<Customer> GetWithInclude(Func<Customer, bool> predicate, params Expression<Func<Customer, object>>[] includeProperties);
+    }
+}
