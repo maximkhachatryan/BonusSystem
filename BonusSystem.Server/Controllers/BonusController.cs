1b3274e7210c075b7304dd53e2fae98f5012fd36
Maxim <maxim.khachatryan.2013@gmail.com>
2020-03-07 00:45:53 +0400

    Commit

--- BonusSystem.Server/Controllers/BonusController.cs
+++ BonusSystem.Server/Controllers/BonusController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BonusSystem.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class BonusController : ControllerBase
     {
@@ -22,31 +22,6 @@ namespace BonusSystem.Server.Controllers
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
